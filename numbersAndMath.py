22a9d85ac04f4e7e782f3f97277fc2bad60ee61d
jonesea7 <edmundalyn@gmail.com>
2020-09-13 11:53:37 -0400

    Add some strs and calculations to find out what each symbol does

--- numbersAndMath.py
+++ numbersAndMath.py
@@ -6,6 +6,9 @@ print("I will now count my chickens: ")
 print("Hens", 25 + 30 / 6)
 print("Roosters", 100 - 25 * 3 % 4)
 
+print("What is 3 % 4? " , 3 % 4)
+print("What is (25 * 3) % 4? " , (25*3)%4)
+
 print("Now I will count the eggs: ")
 
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
@@ -18,3 +21,9 @@ print("What is 3 + 2? ", 3 + 2)
 print("What is 5 - 7?", 5 - 7)
 
 print("Oh, that's why it's False.")
+
+print("How about some more. Let's look at 5 compared to 2!")
+
+print("Is it greater? ", 5 > 2)
+print("Is it greater or equal? ", 5 >= 2)
+print("Is it less or equal? ", 5<=2)
